626915255a718aeb9bded1b0aaaeea5dc527d552
Kaofang <benyapa_kh@cmu.ac.th>
2021-09-02 14:34:55 +0700

    update colorpicked

--- src/components/SelectColorButton.tsx
+++ src/components/SelectColorButton.tsx
@@ -1,18 +1,24 @@
+import { PixelPainterStore } from "../stores/PixelPainterStore"
+
 type SelectColorButtonProps = {
   color: string;
 }
 
 const SelectColorButton = ({ color }: SelectColorButtonProps) => {
 
+  const state = PixelPainterStore.useState()
   //modify this function to highlight correctly
   const computeRingSize = () => {
+    if (color === state.selectedcolor) {
+      return "ring-8 ring-green-400"
+    }
     return ""
-    return "ring-8 ring-green-400"
   }
 
   return (
     <div className={`${computeRingSize()} rounded-md border-black border-2 w-12 h-12 cursor-pointer`}
       style={{ backgroundColor: color }}
+      onClick={() => PixelPainterStore.update(state => {state.selectedcolor = color})}
     >
     </div>
   )
