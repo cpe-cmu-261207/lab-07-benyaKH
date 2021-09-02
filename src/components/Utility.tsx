de285ead92afd3ffa398d58f8a1ff909aa6a84b8
Kaofang <benyapa_kh@cmu.ac.th>
2021-09-02 14:59:24 +0700

    update clear

--- src/components/Utility.tsx
+++ src/components/Utility.tsx
@@ -1,7 +1,12 @@
+import { createEmptyCanvas ,PixelPainterStore } from "../stores/PixelPainterStore"
+
 const Utility = () => {
+  const state = PixelPainterStore.useState()
   return (
     <div className="flex justify-center space-x-3">
-      <button className="w-36">Clear</button>
+      <button className="w-36"
+      onClick={() => PixelPainterStore.update(state => {state.canvas = createEmptyCanvas()})}>
+        Clear</button>
       <button className="w-36">Random color</button>
     </div>
   )
