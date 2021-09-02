f2aca415c9d3b8a0976e7050ed82f7d472efe3b7
Kaofang <benyapa_kh@cmu.ac.th>
2021-09-02 15:17:09 +0700

    update randomcolor

--- src/stores/PixelPainterStore.ts
+++ src/stores/PixelPainterStore.ts
@@ -1,18 +1,37 @@
-import {Store} from 'pullstate'
+import { Store } from 'pullstate'
 
 type PixelPainterStoreType = {
   //we save painted color as hex code (string) in 2D array
-  canvas: string[][] ;
-  selectedcolor: string ;
+  canvas: string[][];
+  selectedcolor: string;
 }
+//return random color
+const randomColor = () => {
+  let color = ['#000000', '#804000', '#FE0000', '#FE6A00', '#FFD800', '#00FF01'
+    , '#FFFFFF', '#01FFFF', '#0094FE', '#0026FF', '#B100FE', '#FF006E']
+  let x = Math.floor((Math.random() * 12))
+  return color[x]
 
-//return an (16 x 16) 2D array filled with "#FFFFFF"
+}
+//return an (16 x 16) 2D array filled with '#FFFFFF'
 export const createEmptyCanvas = () => {
   const output: string[][] = []
-  for (let i=0; i<16; i++){
+  for (let i = 0; i < 16; i++) {
     output[i] = []
-    for (let j=0; j<16; j++){
+    for (let j = 0; j < 16; j++) {
       output[i].push('#FFFFFF')
+    }
+  }
+  return output
+}
+
+export const createRandomCanvas = () => {
+  const output: string[][] = []
+  for (let i = 0; i < 16; i++) {
+    output[i] = []
+    for (let j = 0; j < 16; j++) {
+      let c = randomColor()
+      output[i].push(c);
     }
   }
   return output
@@ -20,5 +39,5 @@ export const createEmptyCanvas = () => {
 
 export const PixelPainterStore = new Store<PixelPainterStoreType>({
   canvas: createEmptyCanvas(),
-  selectedcolor: "#000000",
+  selectedcolor: '#000000',
 })
